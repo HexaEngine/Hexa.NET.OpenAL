4659d4300a6129ecb7ab2e02ee9b84b11e57d280
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-10-28 15:18:47 +0100

    Add project files.

---
+++ Generator/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HexaGen;
+using HexaGen.Patching;
+
+CsCodeGeneratorConfig config = CsCodeGeneratorConfig.Load("generator.json");
+CsCodeGenerator generator = new(config);
+generator.PatchEngine.RegisterPrePatch(new NamingPatch(["Alc", "Al"], NamingPatchOptions.None));
+generator.LogToConsole();
+generator.Generate(["include/main.h"], "../../../../Hexa.NET.OpenAL/Generated");
---
+++ Hexa.NET.OpenAL/OpenAL.cs
@@ -0,0 +1,21 @@
+﻿namespace Hexa.NET.OpenAL
+{
+    using System.Runtime.InteropServices;
+
+    public static partial class OpenAL
+    {
+        static OpenAL()
+        {
+            InitApi();
+        }
+
+        public static string GetLibraryName()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return "OpenAL32";
+            }
+            return "libopenal";
+        }
+    }
+}
